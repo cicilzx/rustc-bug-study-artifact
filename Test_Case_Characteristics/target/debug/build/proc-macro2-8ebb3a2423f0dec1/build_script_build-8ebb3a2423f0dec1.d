ecd1ad9fab57da9695ed9cd424507806e9171209
Zixi Liu <997582313@qq.com>
2025-06-25 15:07:19 +0800

    cargo clean

--- Test_Case_Characteristics/target/debug/build/proc-macro2-8ebb3a2423f0dec1/build_script_build-8ebb3a2423f0dec1.d
+++
@@ -1,5 +0,0 @@
-/home/cici/rustc-bug-study-artifact/Test_Case_Characteristics/target/debug/build/proc-macro2-8ebb3a2423f0dec1/build_script_build-8ebb3a2423f0dec1: /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.93/build.rs
-
-/home/cici/rustc-bug-study-artifact/Test_Case_Characteristics/target/debug/build/proc-macro2-8ebb3a2423f0dec1/build_script_build-8ebb3a2423f0dec1.d: /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.93/build.rs
-
-/home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.93/build.rs:
--- Test_Case_Characteristics/target/debug/deps/csv_core-8474528841fbd4b7.d
+++
@@ -1,7 +0,0 @@
-/home/cici/rustc-bug-study-artifact/Test_Case_Characteristics/target/debug/deps/libcsv_core-8474528841fbd4b7.rmeta: /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/lib.rs /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/reader.rs /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/writer.rs
-
-/home/cici/rustc-bug-study-artifact/Test_Case_Characteristics/target/debug/deps/csv_core-8474528841fbd4b7.d: /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/lib.rs /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/reader.rs /home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/writer.rs
-
-/home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/lib.rs:
-/home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/reader.rs:
-/home/cici/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/csv-core-0.1.12/src/writer.rs:
